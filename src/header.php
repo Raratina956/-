0ac8f763f65cbde171238f152213015b82098e08
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-07 12:47:03 +0900

    通知画像追加

--- src/header.php
+++ src/header.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <img src="image/" alt="ロゴ" title="SpotLink">
-    <a href="通知画面"><img src="" alt="通知"></a>
+    <img src="img/icon.png" alt="ロゴ" title="SpotLink">
+    <a href="info.php"><img src="" alt="通知"></a>
     <!-- ここにハンバーガーメニュー -->
 </body>
 </html>
