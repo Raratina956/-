85a48f3638b44611b17d6b51b759f616dc3dfc30
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-07 17:26:54 +0900

    auto-login

--- src/parts/auto-login.php
+++ src/parts/auto-login.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 require 'parts/db-connect.php';
-
+unset($SESSION['login']);
 if (!(isset($_SESSION['user']))) {
+    $SESSION['login']['error']='ログインをしてください';
     $redirect_url = 'https://aso2201203.babyblue.jp/Nomodon/src/login.php';
     header("Location: $redirect_url");
     exit();
