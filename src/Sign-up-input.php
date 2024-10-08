82d4e9bea340b9954742e6b22c8587a11e58ef1a
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-10-08 13:41:26 +0900

    a

--- src/Sign-up-input.php
+++ src/Sign-up-input.php
@@ -14,19 +14,29 @@
     <h2>新規会員登録</h2>
     <br>
     <form action="Sign-up-output.php" method="post">
-        メールアドレス：<input type="text" name="mail_address" required>
-        <br>
-        分類：<select name="type">
-           		<option value="0">学生</option>
-           		<option value="1">教師</option>
-           	</select>
-        <p>
-        名前：<input type="text" name="name" required>
-        <p>
-        パスワード：<input type="text" name="password" required>
-        <p>
-        パスワード(確認用)：<input type="text" name="confirm_password" required> <!-- フィールド名を変更 -->
-        <p>
+        <div class="form-group">
+            <label for="mail_address">メールアドレス：</label>
+            <input type="email" name="mail_address" id="mail_address" required>
+        </div>
+        <div class="form-group">
+            <label for="type">分類：</label>
+            <select name="type" id="type">
+                <option value="0">学生</option>
+                <option value="1">教師</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="name">名前：</label>
+            <input type="text" name="name" id="name" required>
+        </div>
+        <div class="form-group">
+            <label for="password">パスワード：</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <div class="form-group">
+            <label for="confirm_password">パスワード(確認用)：</label>
+            <input type="password" name="confirm_password" id="confirm_password" required>
+        </div>
         <input type="submit" value="登録">
     </form>
 </body>
