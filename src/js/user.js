3ffa7ecfc6a8f2915cdc311737f208940f8b7314
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-08 14:59:08 +0900

    js

--- src/js/user.js
+++ src/js/user.js
@@ -1,44 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-
-const app = express();
-app.use(bodyParser.json());
-
-const db = mysql.createConnection({
-    host: 'mysql310.phy.lolipop.lan',
-    user: 'LAA1516821',
-    password: 'nomodon',
-    database: 'LAA1516821-spotlink'
-});
-
-db.connect(err => {
-    if (err) throw err;
-    console.log('MySQL connected...');
-});
-
-app.post('/follow', (req, res) => {
-    const userId = req.body.userId; // 認証されたユーザーIDを取得
-    const followedId = req.body.followedId;
-    const isFollowing = req.body.following;
-
-    if (isFollowing) {
-        // フォロー
-        const sql = 'INSERT INTO relationships (follower_id, followed_id) VALUES (?, ?)';
-        db.query(sql, [userId, followedId], (err, result) => {
-            if (err) throw err;
-            res.sendStatus(200);
-        });
-    } else {
-        // フォロー解除
-        const sql = 'DELETE FROM relationships WHERE follower_id = ? AND followed_id = ?';
-        db.query(sql, [userId, followedId], (err, result) => {
-            if (err) throw err;
-            res.sendStatus(200);
-        });
-    }
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+document.getElementById('followButton').addEventListener('click', function() {
+    const isFollowing = this.classList.toggle('following');
+    fetch('/follow', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            userId: 1, // 認証されたユーザーIDを設定
+            followedId: 2, // フォロー対象のユーザーIDを設定
+            following: isFollowing 
+        })
+    });
 });
