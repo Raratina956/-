3c4548b00f8ef770602ee7822cbd751d3ad773ca
megumi0303 <2201179@s.asojuku.ac.jp>
2024-10-08 10:23:07 +0900

    Update login.php

--- src/login.php
+++ src/login.php
@@ -39,7 +39,7 @@ if (isset($_POST['mail'], $_POST['pass'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
     <title>Document</title>
 </head>
 
