cfb03ed063f470f40d5465ec5bb2e7a2a3642606
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-08 12:55:34 +0900

    a

--- src/user.php
+++ src/user.php
@@ -9,16 +9,17 @@
     $iconStmt=$pdo->prepare('select icon_name from Icon where user_id=?');
     $iconStmt->execute([$_SESSION['user']['user_id']]);
     $icon = $iconStmt->fetch(PDO::FETCH_ASSOC);
-    
+    echo 'a';
     //タグ情報を「$_SESSION['user']['user_id']」を使って持ってくる
     $attribute=$pdo->prepare('select * from Tag_attribute where user_id=?');
     $attribute->execute([$_SESSION['user']['user_id']]);
     foreach($attribute as $tag_attribute){
         $tagStmt=$pdo->prepare('select * from Tag_list where tag_id=?');
         $tagStmt->execute([$tag_attribute['tag_id']]);
-
+        echo 'b';
         //DBから持ってきたユーザー情報を「$user」に入れる
         foreach($users as $user){
+            echo 'c';
             //アイコン表示
             echo '<img src="', $icon['icon_name'], '">';
 
