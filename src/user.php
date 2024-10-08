798c9dc09b5022fe04acc4b8b32e653c3783765e
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-08 10:42:15 +0900

    アイコン

---
+++ src/user.php
@@ -0,0 +1,13 @@
+<?php require 'parts/autologin.php'; ?>
+<?php require 'header.php'; ?>
+<?php
+    echo '<img src="">';
+    $users=$pdo->prepare('select * from Users where user_id=?');
+    $users->execute([$_SESSION['user']['user_id']]);
+    foreach($users as $user){   //DBから持ってきたユーザー情報を「$user」に入れる
+        echo '<img src="', $user['icon_name'], '">';
+        echo '';
+    }
+    echo 'ユーザーID：',$_SESSION['user']['user_id'];
+    echo 'ユーザー名：',$_SESSION['user']['user_name'];
+?>
