08298c38eee183d23d311257115dc5a29c7e222c
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-10-08 14:47:20 +0900

    floor

--- src/floor.php
+++ src/floor.php
@@ -1,3 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>クラスルーム</title>
+    <link rel="stylesheet" href="css/floor.css"> <!-- floor.cssをリンク -->
+</head>
+
 <?php
 require 'parts/auto-login.php';
 require 'header.php';
