cd862897e5d0b8d328f446d9d7cf43ed7179380b
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-10-08 13:46:06 +0900

    ff

--- src/Sign-up-input.php
+++ src/Sign-up-input.php
@@ -4,15 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/sign-up-input.css">
-    <title>ログイン</title>
+    <title>新規会員登録</title>
+    <link rel="stylesheet" href="sign-up-input.css">
 </head>
 
 <body>
-    <center>
     <a href="login.php">戻る</a>
     <h2>新規会員登録</h2>
-    <br>
     <form action="Sign-up-output.php" method="post">
         <div class="form-group">
             <label for="mail_address">メールアドレス：</label>
