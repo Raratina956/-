5a8835268ece3a2a36697e8fd9e71e033ad44e74
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-08 13:00:06 +0900

    tag

--- src/user.php
+++ src/user.php
@@ -9,30 +9,30 @@
     $iconStmt=$pdo->prepare('select icon_name from Icon where user_id=?');
     $iconStmt->execute([$_SESSION['user']['user_id']]);
     $icon = $iconStmt->fetch(PDO::FETCH_ASSOC);
-    echo 'a';
+
+    //DBから持ってきたユーザー情報を「$user」に入れる
+    foreach($users as $user){
+        //アイコン表示
+        echo '<img src="', $icon['icon_name'], '">';
+
+        //ユーザー名
+        if($user['s_or_t'] == 0){
+            //生徒(名前、クラス、メールアドレス)
+            echo $user['user_name'];
+            echo 'クラス：';
+        }else{
+            //先生
+            echo $user['user_name'], "先生";
+        }
+    }
+
     //タグ情報を「$_SESSION['user']['user_id']」を使って持ってくる
     $attribute=$pdo->prepare('select * from Tag_attribute where user_id=?');
     $attribute->execute([$_SESSION['user']['user_id']]);
     foreach($attribute as $tag_attribute){
         $tagStmt=$pdo->prepare('select * from Tag_list where tag_id=?');
         $tagStmt->execute([$tag_attribute['tag_id']]);
-        echo 'b';
-        //DBから持ってきたユーザー情報を「$user」に入れる
-        foreach($users as $user){
-            echo 'c';
-            //アイコン表示
-            echo '<img src="', $icon['icon_name'], '">';
 
-            //ユーザー名
-            if($user['s_or_t'] == 0){
-                //生徒(名前、クラス、メールアドレス)
-                echo $user['user_name'];
-                echo 'クラス：';
-            }else{
-                //先生
-                echo $user['user_name'], "先生";
-            }
-        }
         //タグ一覧
         foreach($tagStmt as $tag){
             echo $tag;
