5489cde01addd711968b8588333d8247f1b4fc2f
megumi0303 <2201179@s.asojuku.ac.jp>
2024-10-08 10:09:43 +0900

    Update login.php

--- src/login.php
+++ src/login.php
@@ -39,11 +39,12 @@ if (isset($_POST['mail'], $_POST['pass'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="login.css">
     <title>Document</title>
 </head>
 
 <body>
+<img src="img/icon2.png" class="icon" width="300" height="80">
     <form action="login.php" method="post">
         <span>メールアドレス</span><input type="mail" name="mail" required>
         <br>
