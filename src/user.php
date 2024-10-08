0517bc4095576894182cdaf46a4ba4b34c081f87
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-08 13:08:03 +0900

    d

--- src/user.php
+++ src/user.php
@@ -29,10 +29,10 @@
     //タグ情報を「$_SESSION['user']['user_id']」を使って持ってくる
     $attribute=$pdo->prepare('select * from Tag_attribute where user_id=?');
     $attribute->execute([$_SESSION['user']['user_id']]);
-    echo 'a';
     foreach($attribute as $tag_attribute){
         $tagStmt=$pdo->prepare('select * from Tag_list where tag_id=?');
         $tagStmt->execute([$tag_attribute['tag_id']]);
+        echo 'a';
 
         //タグ一覧
         echo 'タグ一覧<br>';
