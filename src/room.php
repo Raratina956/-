88cef9c19162fdeaace86d232b1576938f32a02b
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-08 11:11:49 +0900

    room

--- src/room.php
+++ src/room.php
@@ -9,7 +9,7 @@ $row = $sql->fetch();
 $room_name = $row['classroom_name'];
 $floor = $row['classroom_floor'];
 if (isset($_POST['judge'])) {
-    $sql_room = $pdo->prepare('SELECT * FROM Classroom WHERE user_id=?');
+    $sql_room = $pdo->prepare('SELECT * FROM Current_location WHERE user_id=?');
     $sql_room->execute([$_SESSION['user']['user_id']]);
     $row_room = $sql_romm->fetch();
     if (!$row_romm) {
