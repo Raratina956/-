5c189f8212aed10684cffe847dc76d4467c4d3fa
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-08 10:15:35 +0900

    room作成

--- src/floor.php
+++ src/floor.php
@@ -5,4 +5,17 @@ require 'parts/auto-login.php';
 <?php
 require 'header.php';
 ?>
-階数表示
+<?php
+$floor = $_POST['floor'];
+echo '<h1>', $floor, '階</h1>';
+$sql = $pdo->prepare('SELECT * FROM Classroom WHERE classroom_floor = ?');
+$sql->execute([$floor]);
+$rows = $sql->fetchAll();
+echo '<ul>';
+foreach ($rows as $row) {
+    echo '<li>';
+    echo '<a herf="room.php?id=',$row['floor_id'],'>',$row['floor_name'],'</a>';
+    echo '</li>';
+}
+echo '</ul>';
+
