fd54444e2ac9136f995e26a0a3808449a82e627c
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-08 12:08:59 +0900

    floor

--- src/floor.php
+++ src/floor.php
@@ -11,10 +11,15 @@ echo '<h1>', $floor, '階</h1>';
 $sql = $pdo->prepare('SELECT * FROM Classroom WHERE classroom_floor = ?');
 $sql->execute([$floor]);
 $rows = $sql->fetchAll();
+$classroom_id = $row['classroom_id'];
+$classroom_name = $row['classroom_name'];
+$sql_current = $pdo->prepare('SELECT classroom_id, COUNT(*) AS user_count FROM Current_location GROUP BY classroom_id');
+$sql_current->execute();
+$results = $sql_current->fetchAll(PDO::FETCH_ASSOC);
 echo '<ul>';
 foreach ($rows as $row) {
     echo '<li>';
-    echo '<a href="room.php?id=',$row['classroom_id'],'">',$row['classroom_name'],'</a>';
+    echo '<a href="room.php?id=',$classroom_id,'">',$classroom_name,'　',$row['user_count'],'人</a>';
     echo '</li>';
 }
 echo '</ul>';
