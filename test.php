9dbe266658bd722c35a4cbd372c95f54c5430f7b
Him-0 <2201194@s.asojuku.ac.jp>
2024-10-08 09:46:50 +0900

    a

---
+++ test.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/test.css">
+    <title>Document</title>
+</head>
+<body>
+  <header>
+    <div class="header-area">
+      <h1>ロゴ</h1>
+      <div class="hamburger">
+        <!-- ハンバーガーメニューの線 -->
+        <span></span>
+        <span></span>
+        <span></span>
+        <!-- /ハンバーガーメニューの線 -->
+      </div>
+    </div>
+    <ul class="slide-menu">
+      <li>メニュー</li>
+      <li>メニュー2</li>
+      <li>メニュー3</li>
+      <li>メニュー4</li>
+    </ul>
+  </header>
+</body>
+</html>
