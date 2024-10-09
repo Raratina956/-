73e47d65312df8f319308755e598e4659c87368d
Him-0 <2201194@s.asojuku.ac.jp>
2024-10-09 10:53:23 +0900

    k

--- src/my_tag.php
+++ src/my_tag.php
@@ -8,19 +8,11 @@ if (isset($_POST['tag_name'])) {
         $_SESSION['user']['user_id']
     ]);
 }
-if (isset($_POST['delete_tag_id'])) {
-    $delete_tag_id = $_POST['delete_tag_id'];
-    $sql_delete = $pdo->prepare('DELETE FROM Tag_attribute WHERE tag_id=?');
-    $sql_delete->execute([$delete_tag_id]);
-    $sql_delete = $pdo->prepare('DELETE FROM Tag_list WHERE tag_id=?');
-    $sql_delete->execute([$delete_tag_id]);
-}
 ?>
 
 <?php
 // require 'header.php';
 ?>
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -30,15 +22,6 @@ if (isset($_POST['delete_tag_id'])) {
     <link rel="stylesheet" href="css/my_tag.css">
     <title>Document</title>
 </head>
-<body>
-    <h1>ｍｙタグ一覧</h1>
-    <h2>タグ作成</h2>
-    <form action="my_tag.php" method="post">
-        タグ名：
-        <input type="name" name="tag_name">
-        <input type="submit" value="作成">
-    </form>
-=======
 <h1>ｍｙタグ一覧</h1>
 <h2>タグ作成</h2>
 <form action="my_tag.php" method="post">
@@ -68,7 +51,7 @@ if ($list_raw) {
                 <td><input type="submit" value="更新"></td>
             </form>
             <form action="my_tag.php" method="post">
-                <input type="hidden" name="delete_tag_id" value=<?php echo $row['tag_id']; ?>>
+                <input type="hidden" name="tag_id" value=<?php echo $row['tag_id']; ?>>
                 <td><input type="submit" value="削除"></td>
             </form>
             <?php
@@ -76,41 +59,9 @@ if ($list_raw) {
         }
         ?>
     </table>
->>>>>>> 9772213ef79da4d255469c943f774fe20d7eb631
     <?php
-    $list_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE user_id=?');
-    $list_sql->execute([$_SESSION['user']['user_id']]);
-    $list_raw = $list_sql->fetchAll(PDO::FETCH_ASSOC);
-    if ($list_raw) {
-        ?>
-        <table>
-            <th>タグID</th>
-            <th>タグ名</th>
-            <th></th>
-            <th></th>
-            <?php
-            foreach ($list_raw as $row) {
-                echo '<tr>';
-                echo '<td>', $row['tag_id'], '</td>';
-                echo '<td>', $row['tag_name'], '</td>';
-                ?>
-                <form action="tag_update.php" method="post">
-                    <input type="hidden" name="tag_id" value=<?php echo $row['tag_id']; ?>>
-                    <td><input type="submit" value="更新"></td>
-                </form>
-                <form action="my_tag.php" method="post">
-                    <input type="hidden" name="tag_id" value=<?php echo $row['tag_id']; ?>>
-                    <td><input type="submit" value="削除"></td>
-                </form>
-                <?php
-                echo '</tr>';
-            }
-            ?>
-        </table>
-        <?php
-    } else {
-        echo '作成されたタグがありません';
-    }
-    ?>
-    <a href="main.php">メインへ</a>
-</body>
+} else {
+    echo '作成されたタグがありません';
+}
+?>
+<a href="main.php">メインへ</a>
