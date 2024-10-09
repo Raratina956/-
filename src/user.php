02c9bf46cb4472cf7905a85b69016a14533325ce
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-09 12:16:03 +0900

    follow

--- src/user.php
+++ src/user.php
@@ -93,7 +93,7 @@
                         <input type="hidden" name="user_id" value=', $_GET['user_id'], '>
                         <input type="hidden" name="action" value="follow">
                         <button type="submit">
-                            <img src="img\star.png" width="10%" height="10%">
+                            <img src="img\notstar.png" width="10%" height="10%">
                         </button>
                       </form><br>';
             }
