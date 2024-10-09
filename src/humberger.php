7ad40ad6eea078366339ce30faf40161b53d41c3
Him-0 <2201194@s.asojuku.ac.jp>
2024-10-09 09:45:19 +0900

    Update humberger.php

--- src/humberger.php
+++ src/humberger.php
@@ -24,7 +24,6 @@
         <div class="slide-menu">
             <!-- ユーザー情報の表示（アイコンは省略） -->
             <div class="user-info">
-                <p>鈴研 イチロー</p>
                 <input type="text" class="search-box" placeholder="先生or生徒を検索">
             </div>
             <!-- メニューリスト -->
