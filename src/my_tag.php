b957cc46f2f39770f5e3e56f9d7d22223c2bb6c6
Him-0 <2201194@s.asojuku.ac.jp>
2024-10-09 10:50:33 +0900

    a

--- src/my_tag.php
+++ src/my_tag.php
@@ -13,46 +13,57 @@ if (isset($_POST['tag_name'])) {
 <?php
 // require 'header.php';
 ?>
-<h1>ｍｙタグ一覧</h1>
-<h2>タグ作成</h2>
-<form action="my_tag.php" method="post">
-    タグ名：
-    <input type="name" name="tag_name">
-    <input type="submit" value="作成">
-</form>
-<?php
-$list_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE user_id=?');
-$list_sql->execute([$_SESSION['user']['user_id']]);
-$list_raw = $list_sql->fetchAll(PDO::FETCH_ASSOC);
-if ($list_raw) {
-    ?>
-    <table>
-        <th>タグID</th>
-        <th>タグ名</th>
-        <th></th>
-        <th></th>
-        <?php
-        foreach ($list_raw as $row) {
-            echo '<tr>';
-            echo '<td>', $row['tag_id'], '</td>';
-            echo '<td>', $row['tag_name'], '</td>';
-            ?>
-            <form action="tag_update.php" method="post">
-                <input type="hidden" name="tag_id" value=<?php echo $row['tag_id']; ?>>
-                <td><input type="submit" value="更新"></td>
-            </form>
-            <form action="my_tag.php" method="post">
-                <input type="hidden" name="tag_id" value=<?php echo $row['tag_id']; ?>>
-                <td><input type="submit" value="削除"></td>
-            </form>
-            <?php
-            echo '</tr>';
-        }
-        ?>
-    </table>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/my_tag.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ｍｙタグ一覧</h1>
+    <h2>タグ作成</h2>
+    <form action="my_tag.php" method="post">
+        タグ名：
+        <input type="name" name="tag_name">
+        <input type="submit" value="作成">
+    </form>
     <?php
-} else {
-    echo '作成されたタグがありません';
-}
-?>
-<a href="main.php">メインへ</a>
+    $list_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE user_id=?');
+    $list_sql->execute([$_SESSION['user']['user_id']]);
+    $list_raw = $list_sql->fetchAll(PDO::FETCH_ASSOC);
+    if ($list_raw) {
+        ?>
+        <table>
+            <th>タグID</th>
+            <th>タグ名</th>
+            <th></th>
+            <th></th>
+            <?php
+            foreach ($list_raw as $row) {
+                echo '<tr>';
+                echo '<td>', $row['tag_id'], '</td>';
+                echo '<td>', $row['tag_name'], '</td>';
+                ?>
+                <form action="tag_update.php" method="post">
+                    <input type="hidden" name="tag_id" value=<?php echo $row['tag_id']; ?>>
+                    <td><input type="submit" value="更新"></td>
+                </form>
+                <form action="my_tag.php" method="post">
+                    <input type="hidden" name="tag_id" value=<?php echo $row['tag_id']; ?>>
+                    <td><input type="submit" value="削除"></td>
+                </form>
+                <?php
+                echo '</tr>';
+            }
+            ?>
+        </table>
+        <?php
+    } else {
+        echo '作成されたタグがありません';
+    }
+    ?>
+    <a href="main.php">メインへ</a>
+</body>
