1091d76bc8d8f598e4a44110d25b2773b8d202e6
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-10 11:20:18 +0900

    Update map.php

--- src/map.php
+++ src/map.php
@@ -42,7 +42,6 @@
                 for($i = 7;$i>0; $i--){
                     echo '<tr>';
                     echo '<form name="floor" action="floor.php" method="post">';
-                    echo '<a href="./floor.php" onclick="document.floor.submit();>';
                     echo '<td class="block"><div class="box">aaaaa</div></a></td>';
                     echo '<input type="hidden" name="floor" value="', $i, '">';
                     echo '<td class="number"><button type="submit" value="',$i,' name="floor"">',$i,'</td>';
