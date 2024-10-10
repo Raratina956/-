328a87ea0ffc237994ad67c38265a3edba66ac77
Him-0 <2201194@s.asojuku.ac.jp>
2024-10-10 12:26:22 +0900

    Update tag_list.php

--- src/tag_list.php
+++ src/tag_list.php
@@ -23,8 +23,8 @@ require 'header.php';
 ?>
 <link rel="stylesheet" href="css/tag_list.css">
 <h1>みんなのタグ</h1>
-<a href=""><span>参加しているタグはこちら</span></a><br>
-<form action="tag_list" method="post">
+<a href=""><span>参加しているタグはこちら</span></a>
+<br><form action="tag_list" method="post">
     <input type="text" name="tag_search" class="textbox" placeholder="検索したい内容を入力してください">
     <input type="submit" value="検索" class="search"> 
 </form>
