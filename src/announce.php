a72f56e6198ece0254d710973af2bbc37d6614a7
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-11 18:06:26 +0900

    announce

--- src/announce.php
+++ src/announce.php
@@ -12,7 +12,7 @@ if (isset($_POST['content'])) {
         $content,
         $now_time
     ]);
-    $announcement_id=$pdo->lastInsertId();
+    $announcement_id = $pdo->lastInsertId();
     $sql_select = $pdo->prepare("SELECT * FROM Tag_attribute WHERE tag_id=?");
     $sql_select->execute([$tag_id]);
     $results = $sql_select->fetchAll(PDO::FETCH_ASSOC);
@@ -26,35 +26,56 @@ if (isset($_POST['content'])) {
             ]);
         }
     }
+    $sql_tag = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
+    $sql_tag->execute([$tag_id]);
+    $row_tag = $sql_tag->fetch();
+    $tag_name = $row_tag['tag_name'];
 }
 ?>
 <?php
 // require 'header.php';
 ?>
-<h1>アナウンス</h1>
 <?php
-$join_sql = $pdo->prepare("SELECT * FROM Tag_attribute WHERE user_id=?");
-$join_sql->execute([$_SESSION['user']['user_id']]);
-$results = $join_sql->fetchAll(PDO::FETCH_ASSOC);
-if ($results) {
+if (isset($_POST['content'])) {
+    // 下記アナウンス発信前
     ?>
-    <form action="announce.php" method="post">
-        <select name="tag_id">
-            <?php
-            foreach ($results as $join_row) {
-                $tag_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
-                $tag_sql->execute([$join_row['tag_id']]);
-                $tag_row = $tag_sql->fetch(PDO::FETCH_ASSOC);
-                echo '<option value=', $join_row['tag_id'], '>', $tag_row['tag_name'], '</option>';
-            }
-            ?>
-        </select>
-        <textarea name="content"></textarea>
-        <input type="submit" value="送信">
-    </form>
-
+    <h1>アナウンス</h1>
     <?php
+    $join_sql = $pdo->prepare("SELECT * FROM Tag_attribute WHERE user_id=?");
+    $join_sql->execute([$_SESSION['user']['user_id']]);
+    $results = $join_sql->fetchAll(PDO::FETCH_ASSOC);
+    if ($results) {
+        ?>
+        <form action="announce.php" method="post">
+            <select name="tag_id">
+                <?php
+                foreach ($results as $join_row) {
+                    $tag_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
+                    $tag_sql->execute([$join_row['tag_id']]);
+                    $tag_row = $tag_sql->fetch(PDO::FETCH_ASSOC);
+                    echo '<option value=', $join_row['tag_id'], '>', $tag_row['tag_name'], '</option>';
+                }
+                ?>
+            </select>
+            <textarea name="content"></textarea>
+            <br>
+            <input type="submit" value="送信">
+        </form>
+
+        <?php
+    } else {
+        echo 'タグを追加してください';
+    }
+    // 上記アナウンス発信前
 } else {
-    echo 'タグを追加してください';
+    // 下記アナウンス発信後
+    ?>
+    <h1>アナウンス発信しました。</h1>
+    <br>
+    <h2>宛先：<?php echo $tag_name; ?></h2>
+    <br>
+    <h3><?php echo $content; ?></h3>
+    <?php
+    // 上記アナウンス発信後
 }
 ?>
