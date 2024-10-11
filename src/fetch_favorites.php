e16eb7024e2931b4d89f27716a4459f2c01e9f53
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-11 10:27:56 +0900

    favorite

--- src/fetch_favorites.php
+++ src/fetch_favorites.php
@@ -6,16 +6,34 @@ $type = $_POST['type'];
 
 switch ($type) {
     case 'all':
-        $sql = $pdo->prepare('SELECT * FROM Favorite WHERE follow_id=?');
-        $sql->execute([$_SESSION['user']['user_id']]);
+        $all_sql = $pdo->prepare('
+        SELECT Favorite.*, Users.s_or_t, Users.user_name 
+        FROM Favorite 
+        JOIN Users ON Favorite.follower_id = Users.user_id 
+        WHERE Favorite.follow_id = ?
+    ');
+        $all_sql->execute([$_SESSION['user']['user_id']]);
+        $all_list = $all_sql->fetchAll(PDO::FETCH_ASSOC);
         break;
     case 'teacher':
-        $sql = $pdo->prepare('SELECT * FROM Favorite WHERE follow_id=? AND s_or_t=?');
-        $sql->execute([$_SESSION['user']['user_id'], 1]);
+        $student_sql = $pdo->prepare('
+        SELECT Favorite.*, Users.s_or_t, Users.user_name 
+        FROM Favorite 
+        JOIN Users ON Favorite.follower_id = Users.user_id 
+        WHERE Favorite.follow_id = ? AND Users.s_or_t = ?
+    ');
+        $student_sql->execute([$_SESSION['user']['user_id'], 0]);
+        $student_list = $student_sql->fetchAll(PDO::FETCH_ASSOC);
         break;
     case 'student':
-        $sql = $pdo->prepare('SELECT * FROM Favorite WHERE follow_id=? AND s_or_t=?');
-        $sql->execute([$_SESSION['user']['user_id'], 0]);
+        $student_sql = $pdo->prepare('
+        SELECT Favorite.*, Users.s_or_t, Users.user_name 
+        FROM Favorite 
+        JOIN Users ON Favorite.follower_id = Users.user_id 
+        WHERE Favorite.follow_id = ? AND Users.s_or_t = ?
+    ');
+        $student_sql->execute([$_SESSION['user']['user_id'], 0]);
+        $student_list = $student_sql->fetchAll(PDO::FETCH_ASSOC);
         break;
     default:
         echo '不正なリクエスト';
@@ -32,9 +50,9 @@ if ($list_raw) {
         $sql_user = $pdo->prepare('SELECT * FROM Users WHERE user_id=?');
         $sql_user->execute([$favorite['follower_id']]);
         $row_user = $sql_user->fetch();
-        if($row_user['s_or_t'] === 0){
+        if ($row_user['s_or_t'] === 0) {
             echo '<td>', $row_user['user_name'], '</td>';
-        }else{
+        } else {
             echo '<td>', $row_user['user_name'], '　先生</td>';
         }
         echo '</tr>';
