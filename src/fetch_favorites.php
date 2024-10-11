21ce4a14702a0a46cb1cacec855b1785dbb609fc
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-11 10:29:01 +0900

    favoriet

--- src/fetch_favorites.php
+++ src/fetch_favorites.php
@@ -3,58 +3,51 @@ require 'parts/auto-login.php';
 
 // POSTで送られてきたデータを取得
 $type = $_POST['type'];
+$list_raw = []; // 初期化
 
 switch ($type) {
     case 'all':
         $all_sql = $pdo->prepare('
-        SELECT Favorite.*, Users.s_or_t, Users.user_name 
-        FROM Favorite 
-        JOIN Users ON Favorite.follower_id = Users.user_id 
-        WHERE Favorite.follow_id = ?
-    ');
+            SELECT Favorite.*, Users.s_or_t, Users.user_name 
+            FROM Favorite 
+            JOIN Users ON Favorite.follower_id = Users.user_id 
+            WHERE Favorite.follow_id = ?
+        ');
         $all_sql->execute([$_SESSION['user']['user_id']]);
-        $all_list = $all_sql->fetchAll(PDO::FETCH_ASSOC);
+        $list_raw = $all_sql->fetchAll(PDO::FETCH_ASSOC); // データを取得
         break;
     case 'teacher':
-        $student_sql = $pdo->prepare('
-        SELECT Favorite.*, Users.s_or_t, Users.user_name 
-        FROM Favorite 
-        JOIN Users ON Favorite.follower_id = Users.user_id 
-        WHERE Favorite.follow_id = ? AND Users.s_or_t = ?
-    ');
-        $student_sql->execute([$_SESSION['user']['user_id'], 0]);
-        $student_list = $student_sql->fetchAll(PDO::FETCH_ASSOC);
+        $teacher_sql = $pdo->prepare('
+            SELECT Favorite.*, Users.s_or_t, Users.user_name 
+            FROM Favorite 
+            JOIN Users ON Favorite.follower_id = Users.user_id 
+            WHERE Favorite.follow_id = ? AND Users.s_or_t = ?
+        ');
+        $teacher_sql->execute([$_SESSION['user']['user_id'], 1]);
+        $list_raw = $teacher_sql->fetchAll(PDO::FETCH_ASSOC); // データを取得
         break;
     case 'student':
         $student_sql = $pdo->prepare('
-        SELECT Favorite.*, Users.s_or_t, Users.user_name 
-        FROM Favorite 
-        JOIN Users ON Favorite.follower_id = Users.user_id 
-        WHERE Favorite.follow_id = ? AND Users.s_or_t = ?
-    ');
+            SELECT Favorite.*, Users.s_or_t, Users.user_name 
+            FROM Favorite 
+            JOIN Users ON Favorite.follower_id = Users.user_id 
+            WHERE Favorite.follow_id = ? AND Users.s_or_t = ?
+        ');
         $student_sql->execute([$_SESSION['user']['user_id'], 0]);
-        $student_list = $student_sql->fetchAll(PDO::FETCH_ASSOC);
+        $list_raw = $student_sql->fetchAll(PDO::FETCH_ASSOC); // データを取得
         break;
     default:
         echo '不正なリクエスト';
         exit;
 }
 
-$list_raw = $sql->fetchAll(PDO::FETCH_ASSOC);
-
+// 取得したデータを表示
 if ($list_raw) {
     echo '<table>';
     foreach ($list_raw as $favorite) {
         echo '<tr>';
         echo '<td>アイコン（仮）</td>';
-        $sql_user = $pdo->prepare('SELECT * FROM Users WHERE user_id=?');
-        $sql_user->execute([$favorite['follower_id']]);
-        $row_user = $sql_user->fetch();
-        if ($row_user['s_or_t'] === 0) {
-            echo '<td>', $row_user['user_name'], '</td>';
-        } else {
-            echo '<td>', $row_user['user_name'], '　先生</td>';
-        }
+        echo '<td>', $favorite['user_name'], ($favorite['s_or_t'] === 0 ? '' : '　先生'), '</td>';
         echo '</tr>';
     }
     echo '</table>';
