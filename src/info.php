3de627640ea3a890a4f7cc718a1202442918bf07
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-11 18:17:48 +0900

    info作成

---
+++ src/info.php
@@ -0,0 +1,17 @@
+<?php
+require 'parts/auto-login.php';
+?>
+
+<?php
+// require 'header.php';
+?>
+<h1>お知らせ</h1>
+<table>
+    <tr>
+    <td rowspan="2">アイコン</td>
+    <td>名前さんが、アナウンスをしました</td>
+    </tr>
+    <tr>
+        <td>アナウンス内容</td>
+    </tr>
+</table>
