eb80f0e2c90a5bcabb54a221d420d2b7b4a06926
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-11 17:51:44 +0900

    announce

--- src/announce.php
+++ src/announce.php
@@ -1,9 +1,31 @@
 <?php
 require 'parts/auto-login.php';
-if(isset($_POST['content'])){
+if (isset($_POST['content'])) {
     $tag_id = $_POST['tag_id'];
     $content = $_POST['content'];
-    
+    $now_time = date("Y/m/d H:i:s");
+    $send_user_id = $_SESSION['user']['user_id'];
+    $sql_insert = $pdo->prepare('INSERT INTO Notification (send_person,sent_tag,content,sending_time) VALUES (?,?,?,?)');
+    $sql_insert->execute([
+        $send_user_id,
+        $tag_id,
+        $content,
+        $now_time
+    ]);
+    $announcement_id=$pdo->lastInsertId();
+    $sql_select = $pdo->prepare("SELECT * FROM Tag_attribute WHERE tag_id=?");
+    $sql_select->execute([$tag_id]);
+    $results = $sql_select->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($results as $row_user) {
+        $sent_user_id = $row_user['user_id'];
+        if ($send_user_id != $sent_user_id) {
+            $sql_insert = $pdo->prepare('INSERT INTO Announce_check (announcement_id,user_id) VALUES (?,?)');
+            $sql_insert->execute([
+                $announcement_id,
+                $sent_user_id
+            ]);
+        }
+    }
 }
 ?>
 <?php
@@ -23,7 +45,7 @@ if ($results) {
                 $tag_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
                 $tag_sql->execute([$join_row['tag_id']]);
                 $tag_row = $tag_sql->fetch(PDO::FETCH_ASSOC);
-                echo '<option value=',$join_row['tag_id'],'>',$tag_row['tag_name'],'</option>';             
+                echo '<option value=', $join_row['tag_id'], '>', $tag_row['tag_name'], '</option>';
             }
             ?>
         </select>
