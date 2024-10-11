51dbe001b56a1e928fdbc8ea96a5842270a32898
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-11 10:27:17 +0900

    UP user

--- src/user.php
+++ src/user.php
@@ -1,18 +1,19 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/user.css">
-    <title>Document</title>
-   
-</head>
-   
-    <body>
-
 <?php
     require 'parts/auto-login.php';
+?>
 
+    <!DOCTYPE html>
+    <html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/user.css">
+        <title>Document</title>
+       
+    </head>
+       
+        <body>
+<?php
     //フォロー・フォロワー機能
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $follower_id = $_POST['user_id'];
