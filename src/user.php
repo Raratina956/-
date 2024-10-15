c9194848cfd4adf813e100bde6ddf79cf0e7f8c2
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-15 12:19:13 +0900

    user UP

--- src/user.php
+++ src/user.php
@@ -161,7 +161,6 @@
                 echo 'タグ一覧<br>';
                 foreach($tags as $tag){
                     echo '</div>';
-                    echo '</div>';
                     echo '<br>';
                     echo '<div class="tag">';
                     echo $tag['tag_name'];
