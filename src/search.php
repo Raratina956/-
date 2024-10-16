f55bb996cb7a40951507b4918773e48002621ab0
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-16 11:25:11 +0900

    search

--- src/search.php
+++ src/search.php
@@ -24,20 +24,23 @@ if (empty($_POST['a_u_t'])) {
     <select name="a_u_t">
         <option value="a">全て</option>
         <?Php
-        if(isset($_POST['a_u_t'])){
-            if($_POST['a_u_t']=="u"){
+        if (isset($_POST['a_u_t'])) {
+            if ($_POST['a_u_t'] == "u") {
                 echo '<option value="u" selected>ユーザー</option>';
                 echo '<option value="t">タグ</option>';
-            }else if($_POST['a_u_t']=="t"){
+            } else if ($_POST['a_u_t'] == "t") {
                 echo '<option value="u">ユーザー</option>';
                 echo '<option value="t" selected>タグ</option>';
+            } else {
+                echo '<option value="u">ユーザー</option>';
+                echo '<option value="t">タグ</option>';
             }
-        }else{
+        } else {
             echo '<option value="u">ユーザー</option>';
             echo '<option value="t">タグ</option>';
         }
         ?>
-        
+
     </select>
     <input type="submit" value="絞込">
     <input type="hidden" name="search" value="<?php echo $search_text ?>">
