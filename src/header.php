d94e8a0e111838a6a15884f144543fc8e3780721
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-16 10:00:32 +0900

    x

--- src/header.php
+++ src/header.php
@@ -17,7 +17,7 @@ if (isset($_POST['logout'])) {
 </head>
 <header>
 
-    <a href="main.php"><img src="img/icon.png" class="icon" width="300" height="80"></a>
+    <a href="main.php" class="icon"><img src="img/icon.png" width="300" height="80"></a>
     <img src="img/bell.png" class="bell" width="50" height="50">
     <div class="header-area">
         <div class="hamburger">
