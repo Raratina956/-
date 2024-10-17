191a4c8753fcaa411607871fc9fb17d17f323bd2
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-17 11:30:28 +0900

    Update user.php

--- src/user.php
+++ src/user.php
@@ -122,15 +122,15 @@
                         <input type="hidden" name="user_id" value=', $_GET['user_id'], '>
                         <input type="hidden" name="action" value="unfollow">
                         <button type="submit" class="star">
-                            <img src="img\star.png" width="5%" height="5%">
+                            <img src="img\star.png" width="10%" height="10%">
                         </button>
                       </form><br>';
             }else{
                 echo '<form action="user.php" method="post">
                         <input type="hidden" name="user_id" value=', $_GET['user_id'], '>
                         <input type="hidden" name="action" value="follow">
-                        <button type="submit" class="nostar">
-                            <img src="img\notstar.png" width="5%" height="5%" class="star">
+                        <button type="submit">
+                            <img src="img\notstar.png" width="10%" height="10%" class="star">
                         </button>
                       </form><br>';
             }
