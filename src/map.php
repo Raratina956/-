cc922ee219cc6af825734ba2af9d1501c97150f7
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-21 12:12:04 +0900

    Update map.php

--- src/map.php
+++ src/map.php
@@ -46,14 +46,14 @@
                 // 位置情報アイコン
 
                //アイコン情報を持ってくる
-                $iconStmt=$pdo->prepare('select icon_name 
+                $iconStmt=$pdo->prepare('select * 
                                          from Icon
                                          LEFT JOIN Current_location On Icon.user_id = Current_location.user_id
                                          where classroom_id=?');
                 $iconStmt->execute([$i]);
                 $icon = $iconStmt->fetchAll(PDO::FETCH_ASSOC);
                     foreach($icon as $ic){
-                        echo '<img src="', $icon['icon_name'], '" width="20%" height="50%" class="usericon">';
+                        echo '<img src="', $ic['icon_name'], '" width="20%" height="50%" class="usericon">';
                     }
                 echo '</div>';
                 echo '</td>';
