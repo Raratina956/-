b402d24eebfe7e6805e92e96bbfd01aa78a450ac
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-21 10:41:26 +0900

    Update user.php

--- src/user.php
+++ src/user.php
@@ -120,7 +120,7 @@
             echo '<div class="favorite-container">';
             echo '<button type="submit" class="star">';
             echo '<img src="img\chat.png" width="10%" height="10%" class="chat">';
-            echo '</button>'
+            echo '</button>';
 
             //お気に入りボタン表示
             $followStmt=$pdo->prepare('select * from Favorite where follow_id=? and follower_id=?');
