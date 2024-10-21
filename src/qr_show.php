0859dc224700f221ce2bdfbbf49cc86614f8e39e
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-22 00:15:25 +0900

    QR

--- src/qr_show.php
+++ src/qr_show.php
@@ -10,36 +10,17 @@
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['custom_url'])) {
         $customUrl = htmlspecialchars($_POST['custom_url']);
-        echo '<input id="text" type="text" value="' . $customUrl . '" style="width:80%" /><br />';
         echo '<div id="qrcode"></div>';
         echo '送信されたURL: ' . $customUrl;
-        // QRコード生成ロジックをここに追加する
     ?>
         <script src="https://cdn.jsdelivr.net/npm/qrcodejs/qrcode.min.js"></script>
         <script>
-            var qrcode = new QRCode(document.getElementById("qrcode"), {
+            // QRコードを生成
+            new QRCode(document.getElementById("qrcode"), {
+                text: "<?php echo $customUrl; ?>",
                 width: 128,
                 height: 128
             });
-
-            function makeCode() {
-                var elText = document.getElementById("text");
-
-                if (!elText.value) {
-                    elText.focus();
-                    return;
-                }
-
-                // 既存のQRコードをクリア
-                document.getElementById("qrcode").innerHTML = "";
-
-                // 新しいQRコードを生成
-                qrcode.makeCode(elText.value);
-            }
-
-            makeCode();
-
-            document.getElementById("text").addEventListener("input", makeCode);
         </script>
     <?php
     } else {
