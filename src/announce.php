9b2c58bd54c602023dbf0e035ea04528dfcf6fe2
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-22 11:20:50 +0900

    annonce

--- src/announce.php
+++ src/announce.php
@@ -62,6 +62,7 @@ if (!isset($_POST['content'])) {
             </select>
             <br>
             <input type="text" name="title">
+            <br>
             <textarea name="content"></textarea>
             <br>
             <input type="submit" value="送信">
