e585a6dfc9e329d5423406afada2eac148c559e2
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-22 14:09:05 +0900

    Update map.php

--- src/map.php
+++ src/map.php
@@ -44,18 +44,27 @@
                 echo '<form name="floor" action="floor.php" method="post">';
                 echo '<td class="block">';
                 // 位置情報アイコン
-
-               //アイコン情報を持ってくる
-                $iconStmt=$pdo->prepare('select * 
-                                         from Icon
-                                         LEFT JOIN Current_location On Icon.user_id = Current_location.user_id
-                                         LEFT JOIN Current_location On Icon.user_id = Current_location.user_id
-                                         where classroom_id=?');
-                $iconStmt->execute([$i]);
-                $icon = $iconStmt->fetchAll(PDO::FETCH_ASSOC);
-                // アイコン表示
-                    foreach($icon as $ic){
-                        echo '<img src="', $ic['icon_name'], '" width="12%" height=95%" class="usericon">';
+                // 位置取得
+                $floorStmt=$pdo->prepare('select * 
+                                         from Classroom                                    
+                                         where classroom_floor=?');
+                $floorStmt->execute([$i]);
+                $floor = $floorStmt->fetchAll(PDO::FETCH_ASSOC);
+                $class_id = "";
+                    foreach($floor as $f){
+                        $classroom_id = $f['classroom_id'];
+                    
+                        //アイコン情報を持ってくる
+                        $iconStmt=$pdo->prepare('select * 
+                                                from Icon                                         
+                                                LEFT JOIN Current_location On Icon.user_id = Current_location.user_id
+                                                where classroom_id=?');
+                        $iconStmt->execute([$classroom_id]);
+                        $icon = $iconStmt->fetchAll(PDO::FETCH_ASSOC);
+                        // アイコン表示
+                            foreach($icon as $ic){
+                                echo '<img src="', $ic['icon_name'], '" width="12%" height=95%" class="usericon">';
+                            }
                     }
                 echo '</td>';
                 echo '<input type="hidden" name="floor" value=', $i, '>';
