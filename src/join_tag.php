d5c22018fcc8014afd48f93b993b3430a4f56bb1
Him-0 <2201194@s.asojuku.ac.jp>
2024-10-22 14:18:51 +0900

    Update join_tag.php

--- src/join_tag.php
+++ src/join_tag.php
@@ -10,6 +10,8 @@ if (isset($_POST['delete_tag'])) {
 <?php
 require 'header.php';
 ?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="mob_css/j-tag.css" media="screen and (max-width: 480px)">
 <link rel="stylesheet" href="css/join_tag.css" media="screen and (min-width: 1280px)">
 <h1>参加タグ一覧</h1>
