f532efc50101c70f12b3782b3cca74b92c3fa07e
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-23 09:39:01 +0900

    Update map.php

--- src/map.php
+++ src/map.php
@@ -8,8 +8,9 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/map.css" media="screen and (min-width: 1280px)">
+    <!-- <link rel="stylesheet" type="text/css" href="css/map.css" media="screen and (min-width: 1280px)"> -->
     <!-- <link rel="stylesheet" type="text/css" href="css/map-mob.css" media="screen and (max-width: 480px)"> -->
+    <link rel="stylesheet" type="text/css" href="css/map-mob.css" >
 
     <title>Document</title>
    
