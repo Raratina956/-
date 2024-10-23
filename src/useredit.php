99ad2f518cab1173893b48348e5447cf4ed6ffb5
Him-0 <2201194@s.asojuku.ac.jp>
2024-10-23 09:40:27 +0900

    mob

--- src/useredit.php
+++ src/useredit.php
@@ -27,7 +27,8 @@
                 echo '<img id="existingIcon" src="', $icon['icon_name'], '" width="20%" height="50%" class="icon">';
             }
 ?>
-            <link rel="stylesheet" type="text/css" href="css/useredit.css">
+            <link rel="stylesheet" type="text/css" href="mob_css/useredit-mob.css" media="screen and (max-width: 480px)">
+            <link rel="stylesheet" type="text/css" href="css/useredit.css" media="screen and (min-width: 1280px)">
             <input type="file" id="fileInput" name="icon_file" accept=".jpg"><br>
             <img id="preview" src="#" alt="Preview" style="display:none;"><br>
             <input type="hidden" name="user_id" value="<?php echo $_SESSION['user']['user_id']; ?>">
