b49b7ee64cff5934e545e0049e9bbf253305cc37
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-24 10:38:21 +0900

    search

--- src/search.php
+++ src/search.php
@@ -10,35 +10,73 @@ if (isset($_POST['kinds'])) {
 } else {
     $kinds = "a";
 }
+if (isset($_POST['method'])) {
+    $method = $_POST['method'];
+} else {
+    $method = "part";
+}
 if ($kinds == "a" || $kinds == "u") {
-    // 条件指定なし検索ユーザー
-    $search_all_u = $pdo->prepare('SELECT * FROM Users WHERE user_name LIKE ?');
-    $search_all_u->execute(['%' . $search_text . '%']);
-    $search_all_u_re = $search_all_u->fetchAll(PDO::FETCH_ASSOC);
-    if ($search_all_u_re) {
-        // 条件指定なし検索ユーザー(結果あり)
-        foreach ($search_all_u_re as $search_all_u_row) {
-            $user_data[] = [
-                'type' => 'user',
-                'id' => $search_all_u_row['user_id'],
-                'name' => $search_all_u_row['user_name']
-            ];
+    // ユーザー検索
+    // 部分一致
+    if ($method == "part") {
+        $search_all_u = $pdo->prepare('SELECT * FROM Users WHERE user_name LIKE ?');
+        $search_all_u->execute(['%' . $search_text . '%']);
+        $search_all_u_re = $search_all_u->fetchAll(PDO::FETCH_ASSOC);
+        if ($search_all_u_re) {
+            foreach ($search_all_u_re as $search_all_u_row) {
+                $user_data[] = [
+                    'type' => 'user',
+                    'id' => $search_all_u_row['user_id'],
+                    'name' => $search_all_u_row['user_name']
+                ];
+            }
+        }
+    } else {
+        // 完全一致
+        $search_all_u = $pdo->prepare('SELECT * FROM Users WHERE user_name=?');
+        $search_all_u->execute([$search_text]);
+        $search_all_u_re = $search_all_u->fetchAll(PDO::FETCH_ASSOC);
+        if ($search_all_u_re) {
+            // 条件指定なし検索ユーザー(結果あり)
+            foreach ($search_all_u_re as $search_all_u_row) {
+                $user_data[] = [
+                    'type' => 'user',
+                    'id' => $search_all_u_row['user_id'],
+                    'name' => $search_all_u_row['user_name']
+                ];
+            }
         }
     }
 }
 if ($kinds == "a" || $kinds == "t") {
-    // 条件指定なし検索タグ
-    $search_all_t = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_name LIKE ?');
-    $search_all_t->execute(['%' . $search_text . '%']);
-    $search_all_t_re = $search_all_t->fetchAll(PDO::FETCH_ASSOC);
-    if ($search_all_t_re) {
-        // 条件指定なし検索タグ(結果あり)
-        foreach ($search_all_t_re as $search_all_t_row) {
-            $tag_data[] = [
-                'type' => 'tag',
-                'id' => $search_all_t_row['tag_id'],
-                'name' => $search_all_t_row['tag_name']
-            ];
+    if ($method == "part") {
+        // タグ検索
+        // 部分一致
+        $search_all_t = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_name LIKE ?');
+        $search_all_t->execute(['%' . $search_text . '%']);
+        $search_all_t_re = $search_all_t->fetchAll(PDO::FETCH_ASSOC);
+        if ($search_all_t_re) {
+            foreach ($search_all_t_re as $search_all_t_row) {
+                $tag_data[] = [
+                    'type' => 'tag',
+                    'id' => $search_all_t_row['tag_id'],
+                    'name' => $search_all_t_row['tag_name']
+                ];
+            }
+        }
+    } else {
+        // 完全一致
+        $search_all_t = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_name=?');
+        $search_all_t->execute([$search_text]);
+        $search_all_t_re = $search_all_t->fetchAll(PDO::FETCH_ASSOC);
+        if ($search_all_t_re) {
+            foreach ($search_all_t_re as $search_all_t_row) {
+                $tag_data[] = [
+                    'type' => 'tag',
+                    'id' => $search_all_t_row['tag_id'],
+                    'name' => $search_all_t_row['tag_name']
+                ];
+            }
         }
     }
 }
@@ -56,6 +94,10 @@ echo '<link rel="stylesheet" href="css/search.css">';
             <option value="a">全て</option>
             <option value="u">ユーザーのみ</option>
             <option value="t">タグのみ</option>
+        </select>
+        <select name="method">
+            <option value="all">完全一致</option>
+            <option value="part">部分一致</option>
         </select>
         <input type="submit" value="再検索">
     </form>
