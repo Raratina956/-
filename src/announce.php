6cedb5ccadf093d315d6f149c4f249393303ae3c
Him-0 <2201194@s.asojuku.ac.jp>
2024-10-24 11:29:31 +0900

    Update announce.php

--- src/announce.php
+++ src/announce.php
@@ -64,7 +64,7 @@ if (!isset($_POST['title'])) {
                 ?>
             </select>
             <br>
-            <input type="text" name="title">
+            <input type="text" name="title" placeholder="タイトル">
             <br>
             <textarea name="content"></textarea>
             <br>
