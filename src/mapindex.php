65d9462cb0bdc7e41684aa1b30e6a7e1f345dc92
kawamoto0719 <148738656+kawamoto0719@users.noreply.github.com>
2024-10-28 12:08:17 +0900

    a

--- src/mapindex.php
+++ src/mapindex.php
@@ -3,18 +3,15 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>hoge</title>
+    <title>位置情報取得</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script defer>
-        // ボタンクリック時
         $(document).ready(function() {
             $('#send').click(function() {
-                // 位置情報の取得可否により分岐
                 navigator.geolocation.getCurrentPosition(success, fail);
             });
         });
 
-        // 位置情報が取得できた場合
         function success(pos) {
             var date = new Date(pos.timestamp);
             const position = {
@@ -42,28 +39,39 @@
                     "head": position.head,
                     "speed": position.speed
                 },
-                // 通信が成功した場合　
                 success: function(data, dataType) {
-                    alert(data); // サーバーサイドからの返答を表示させてみる
+                    alert(data); // サーバーサイドからの返答を表示
                 },
-                // 通信が失敗した場合
                 error: function() {
                     alert('失敗らしい');
                 }
             });
+
+            // 位置情報をページに表示
+            $('#result').html(`
+                <h3>位置情報</h3>
+                <p>日時: ${position.data}</p>
+                <p>緯度: ${position.lat}</p>
+                <p>経度: ${position.lon}</p>
+                <p>高度: ${position.alt}</p>
+                <p>位置精度: ${position.posacc}</p>
+                <p>高度精度: ${position.altacc}</p>
+                <p>移動方向: ${position.head}</p>
+                <p>速度: ${position.speed}</p>
+            `);
         }
 
-        // 位置情報が取得できなかった場合
         function fail(error) {
-            if (error.code == 1) alert('位置情報を取得する時に許可がない')
-            if (error.code == 2) alert('何らかのエラーが発生し位置情報が取得できなかった。')
-            if (error.code == 3) alert('タイムアウト　制限時間内に位置情報が取得できなかった。')
+            if (error.code == 1) alert('位置情報を取得する時に許可がない');
+            if (error.code == 2) alert('何らかのエラーが発生し位置情報が取得できなかった。');
+            if (error.code == 3) alert('タイムアウト　制限時間内に位置情報が取得できなかった。');
         }
     </script>
 </head>
 
 <body>
-    <button type="button" id="send">ボタン</button>
+    <button type="button" id="send">位置情報取得</button>
+    <div id="result"></div> <!-- 結果を表示するための要素 -->
 </body>
 
 </html>
