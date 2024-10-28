270449236a0e5a251529685d174a9cd8fdde7bc4
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-10-28 12:24:25 +0900

    afd

--- src/search.php
+++ src/search.php
@@ -109,7 +109,7 @@ echo '<link rel="stylesheet" href="css/search.css">';
 
         if (!empty($tag_data)) {
             foreach ($tag_data as $data) {
-                echo '<tr rowspan="2">';
+                echo '<tr rowspan="2" align="center">';
                 // echo '<td class="tag"><h3>タグ</h3></td>';
                 echo '<td class="name"><h3>', htmlspecialchars($data['name'], ENT_QUOTES, 'UTF-8'), '</h3></td>';
                 echo '</tr>';
