d91690950ea093a7e100aa924726af0e670799bf
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-29 21:43:17 +0900

    Update search.php

--- src/search.php
+++ src/search.php
@@ -118,12 +118,11 @@ echo '<link rel="stylesheet" href="css/search.css">';
         if (!empty($user_data)) {
            
             foreach ($user_data as $data) {
-                echo '<form name="form1" action="user.php" method="get">';
                 // アイコン
                 $iconStmt=$pdo->prepare('select icon_name from Icon where user_id=?');
                 $iconStmt->execute([$data['id']]);
                 $icon = $iconStmt->fetch(PDO::FETCH_ASSOC);
-               
+                echo '<form name="form1" action="user.php" method="get">';
                 echo '<tr>';
                 echo '<td class="tag"><a href="javascript:document.form1.submit()"><img src="', $icon['icon_name'], '" class="usericon"></a></td>';
                 echo '<input type="hidden" name="user_id" value="',$data['id'],'">';
