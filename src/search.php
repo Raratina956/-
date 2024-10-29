7723d5fc9c65eaeb22deeb708991832d49e57586
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-10-29 14:05:46 +0900

    wdw

--- src/search.php
+++ src/search.php
@@ -104,7 +104,7 @@ echo '<link rel="stylesheet" href="css/search.css">';
     <h2>【<?php echo htmlspecialchars($search_text, ENT_QUOTES, 'UTF-8'); ?>】の検索結果</h2>
     
     <!-- Search Results -->
-    
+    <div class="table-container">
     <table class="user-table">
         <tr>
             <th colspan="2">ユーザー</th>
@@ -146,5 +146,6 @@ echo '<link rel="stylesheet" href="css/search.css">';
         }
         ?>
     </table>
+    </div>
 </main>
 <a href="main.php" class="back-link">メインへ</a>
