97ecc9d3d630d86f914cc3cc364edf40564f350f
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-10-29 11:06:02 +0900

    asd

--- src/search.php
+++ src/search.php
@@ -104,7 +104,7 @@ echo '<link rel="stylesheet" href="css/search.css">';
     <h2>【<?php echo htmlspecialchars($search_text, ENT_QUOTES, 'UTF-8'); ?>】の検索結果</h2>
     
     <!-- Search Results -->
-     <h2> ユーザー<h2>
+     <h2> ユーザー</h2>
     <table>
         <tr>
             <th>種類</th>
