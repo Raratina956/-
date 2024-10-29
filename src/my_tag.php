636dd81b5bce8b4213db2a91aa3f10efadb95e8c
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-30 00:02:30 +0900

    tag

--- src/my_tag.php
+++ src/my_tag.php
@@ -46,7 +46,7 @@ if ($list_raw) {
                 <input type="hidden" name="tag_id" value=<?php echo $row['tag_id']; ?>>
                 <td><input type="submit" value="更新" class="button_up"></td>
             </form>
-            <form action="delete_tag.php" method="post" onsubmit="return confirmDelete()">
+            <form action="delete_tag.php" method="post">
                 <input type="hidden" name="delete_tag_id" value=<?php echo $row['tag_id']; ?>>
                 <td><input type="submit" value="削除" class="button_del"></td>
             </form>
@@ -61,9 +61,3 @@ if ($list_raw) {
 }
 ?>
 <a href="main.php" class="back-link">メインへ</a>
-
-<script>
-    function confirmDelete() {
-        return confirm("本当に削除しますか？");
-    }
-</script>
