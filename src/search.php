89a237bda57bf2ec470bfe120163d87896d5a561
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-10-29 11:00:11 +0900

    sfsd

--- src/search.php
+++ src/search.php
@@ -119,11 +119,12 @@ echo '<link rel="stylesheet" href="css/search.css">';
             }
             $judge = 1;
         }
+        echo '</table><table>';
 
         if (!empty($tag_data)) {
             foreach ($tag_data as $data) {
                 echo '<tr>';
-                echo '<td>', htmlspecialchars($data['creator_name'], ENT_QUOTES, 'UTF-8'), '</td>';
+                echo '<td><h3>', htmlspecialchars($data['creator_name'], ENT_QUOTES, 'UTF-8'), '</h3></td>';
                 echo '<td><h3>', htmlspecialchars($data['name'], ENT_QUOTES, 'UTF-8'), '</h3></td>';
                 echo '</tr>';
             }
