66f386a9f26a330d38709b78cd89d2c83bef8330
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-10-29 10:49:35 +0900

    pass

--- src/passedit.php
+++ src/passedit.php
@@ -5,13 +5,15 @@ require 'parts/auto-login.php';
 if (isset($_SESSION['err']['pass_err'])) {
     $error_message = $_SESSION['err']['pass_err'];
     echo '<script>alert("'.$error_message.'");</script>';
+    // セッションエラーを消去 
+    unset($_SESSION['err']['pass_err']);
 }
 ?>
 
 
 
 <form action="passedit_output.php" method="post" onsubmit="return confirm('本当に変更しますか？');">
-    現在のパスワード<input type="password" name="pass"><br>
+    メールアドレス<input type="text" name="mail"><br>
     新規パスワード<input type="password" name="newpass"><br>
     確認パスワード<input type="password" name="re_newpass"><br>
     <button type="submit">登録</button>
