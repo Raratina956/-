cee650a989acd0794b49043751a4bdb975431cdb
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-29 09:56:39 +0900

    Update map.php

--- src/map.php
+++ src/map.php
@@ -64,11 +64,13 @@
                             $j = 1;
                                 foreach($icon as $ic){
                                     if($j < 7){
-                                    echo '<img src="', $ic['icon_name'], '" width="12%" height=95%" class="usericon">';
-                                    $j++;
-                                     }
-                                     echo '<img src="iconover.png" width="12%" height=95%" class="usericon">';
-                                     break;
+                                        echo '<img src="', $ic['icon_name'], '" width="12%" height=95%" class="usericon">';
+                                        $j++;
+                                    }else{
+                                        echo '<img src="iconover.png" width="12%" height=95%" class="usericon">';
+                                        break;
+                                    }
+                                    
                                      
                                 }
                         }
