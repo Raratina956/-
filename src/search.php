d2427c91628f2b9491b8b2fb744f1652fc9bbf61
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-10-29 21:54:56 +0900

    Update search.php

--- src/search.php
+++ src/search.php
@@ -120,16 +120,16 @@ echo '<link rel="stylesheet" href="css/search.css">';
             // var_dump($user_data['id']);
            
             foreach ($user_data as $data) {
-                echo '<form name="form1" action="user.php" method="get">';
+                echo '<form name="',$data['name']'" action="user.php" method="get">';
             // アイコン
                 $iconStmt=$pdo->prepare('select icon_name from Icon where user_id=?');
                 $iconStmt->execute([$data['id']]);
                 $icon = $iconStmt->fetch(PDO::FETCH_ASSOC);
                
                 echo '<tr>';
-                echo '<td class="tag"><a href="javascript:document.form1.submit()"><img src="', $icon['icon_name'], '" class="usericon"></a></td>';
+                echo '<td class="tag"><a href="javascript:document.,'$data['name']',.submit()"><img src="', $icon['icon_name'], '" class="usericon"></a></td>';
                 echo '<input type="hidden" name="user_id" value="',$data['id'],'">';
-                echo '<td class="name"><a href="javascript:document.form1.submit()"><h3>', htmlspecialchars($data['name'], ENT_QUOTES, 'UTF-8'), '</h3></A></td>';
+                echo '<td class="name"><a href="javascript:document.,'$data['name']',.submit()"><h3>', htmlspecialchars($data['name'], ENT_QUOTES, 'UTF-8'), '</h3></A></td>';
                 echo '</form>';
                 echo '</tr>';
                
