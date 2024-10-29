36bf0c36dd7a70247d7412e3fb6d1bc7782a8649
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-29 13:57:14 +0900

    ifno

--- src/info.php
+++ src/info.php
@@ -45,20 +45,26 @@ if (isset($message)) {
 
 // 一括既読機能
 if ($_POST['all_read']) {
-    if ($narrow == 0) {
-        switch ($n_user) {
-            case 0:
-                // narrow:0 n_user:0の時
-                $all_read_sql = $pdo->prepare('UPDATE Announce_check SET read_check=? WHERE user_id=?');
-                $all_read_sql->execute([1, $_SESSION['user']['user_id']]);
-                $message = 'narrow:' . $narrow . ' n_user:' . $n_user;
-                break;
-
-            default:
-                # code...
-                break;
-        }
-    }
+    switch ($narrow) {
+        case 0:
+            switch ($n_user) {
+                case 0:
+                    // narrow:0 n_user:0の時
+                    $all_read_sql = $pdo->prepare('UPDATE Announce_check SET read_check=? WHERE user_id=?');
+                    $all_read_sql->execute([1, $_SESSION['user']['user_id']]);
+                    $message = 'narrow:' . $narrow . ' n_user:' . $n_user;
+                    break;
+    
+                default:
+                    # code...
+                    break;
+            }
+            break;
+        
+        default:
+            # code...
+            break;
+    } 
 }
 ?>
 <?php
