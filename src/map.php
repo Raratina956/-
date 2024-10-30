be7931489261e876bc591a112c9078bd95acb9f1
miyakou1122 <per.miyakou1122@gmail.com>
2024-10-30 09:46:48 +0900

    map

--- src/map.php
+++ src/map.php
@@ -1,6 +1,6 @@
-<?php 
-    require 'parts/auto-login.php';
-    require 'header.php'; 
+<?php
+require 'parts/auto-login.php';
+require 'header.php';
 ?>
 
 <meta charset="UTF-8">
@@ -8,97 +8,99 @@
 <link rel="stylesheet" href="mob_css/map-mob.css" media="screen and (max-width: 480px)">
 <link rel="stylesheet" href="css/map.css" media="screen and (min-width: 1280px)">
 
-    <title>MAP</title>
-   
-   
-    <body>
+<title>MAP</title>
 
-          
-        <?php
-        
-            echo '<div class="map">';
-            echo '<h1 class="title">MAP</h1>';
 
-            $sql = $pdo->prepare('SELECT * FROM Tag_attribute WHERE user_id=?');
-            $sql->execute([$_SESSION['user']['user_id']]);
-            $results = $sql->fetchAll(PDO::FETCH_ASSOC);
+<body>
 
-            //プルダウン
-             echo '<select name="list" class="list">';
-                 
-             if(!empty($results)){
 
-                foreach ($results as $tag_list) {
-                
-                    $sql_tag = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
-                    $sql_tag->execute([$tag_list['tag_id']]);
-                    $row_tag = $sql_tag->fetch();
-                    echo "<option value='",$row_tag['tag_id'],"'>",$row_tag['tag_name'],"</option>";
+    <?php
 
-                }
-             }else{
-              echo  '<option value="">-</option>'; 
-             }
+    echo '<div class="map">';
+    echo '<h1 class="title">MAP</h1>';
 
-            echo '</select><br><br>';
+    $sql = $pdo->prepare('SELECT * FROM Tag_attribute WHERE user_id=?');
+    $sql->execute([$_SESSION['user']['user_id']]);
+    $results = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-            //  map
-            echo '<table>';
-            for($i = 7;$i>0; $i--){
-                    echo '<tr>';
-                    echo '<form name="floor" action="floor.php" method="post">';
-                    echo '<td class="block">';
-                    // 位置情報アイコン
-                    // 位置取得 階のIDを取得
-                    $floorStmt=$pdo->prepare('select * 
+    //プルダウン
+    echo '<select name="list" class="list">';
+
+    if (!empty($results)) {
+
+        foreach ($results as $tag_list) {
+
+            $sql_tag = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
+            $sql_tag->execute([$tag_list['tag_id']]);
+            $row_tag = $sql_tag->fetch();
+            echo "<option value='", $row_tag['tag_id'], "'>", $row_tag['tag_name'], "</option>";
+
+        }
+    } else {
+        echo '<option value="">-</option>';
+    }
+
+    echo '</select><br><br>';
+
+    //  map
+    echo '<table>';
+    for ($i = 7; $i > 0; $i--) {
+        echo '<tr>';
+        echo '<form name="floor" action="floor.php" method="post">';
+        echo '<td class="block">';
+        // 位置情報アイコン
+        // 位置取得 階のIDを取得
+        $floorStmt = $pdo->prepare('select * 
                                             from Classroom                                    
                                             where classroom_floor=?');
-                    $floorStmt->execute([$i]);
-                    $floor = $floorStmt->fetchAll(PDO::FETCH_ASSOC);
-                    $class_id = "";
-                    $j = 0;
+        $floorStmt->execute([$i]);
+        $floor = $floorStmt->fetchAll(PDO::FETCH_ASSOC);
+        $class_id = "";
+        $j = 0;
 
-                        foreach($floor as $f){
-                            $classroom_id = $f['classroom_id'];
-                        
-                            //アイコン情報を持ってくる
-                            $iconStmt=$pdo->prepare('select * 
+        foreach ($floor as $f) {
+            $classroom_id = $f['classroom_id'];
+
+            //アイコン情報を持ってくる
+            $iconStmt = $pdo->prepare('select * 
                                                     from Icon                                         
                                                     LEFT JOIN Current_location On Icon.user_id = Current_location.user_id
                                                     where classroom_id=?');
-                            $iconStmt->execute([$classroom_id]);
-                            $icon = $iconStmt->fetchAll(PDO::FETCH_ASSOC);
-                            // アイコン表示
-                          
-                                foreach($icon as $ic){
-                                    if($j >= 7){
-                                        // 7以上は表示しない
-                                        echo '<img src="img/iconover.png" width="12%" height=95%" class="usericon">';
-                                        break;
-                                    }
-                                    echo '<img src="', $ic['icon_name'], '" width="12%" height=95%" class="usericon">';
-                                    
-                                    $j++;
-                                    
-                                }
-                        }
+            $iconStmt->execute([$classroom_id]);
+            $icon = $iconStmt->fetchAll(PDO::FETCH_ASSOC);
+            // アイコン表示
+            echo '<a href="user.php?user_id=2">';
+            foreach ($icon as $ic) {
 
-                    echo '</td>';
-                    echo '<input type="hidden" name="floor" value=', $i, '>';
-                    echo '<td class="number"><button type="submit" class="floor" value="',$i,'" name="floor">',$i,'階</td>';
-                    echo '</tr>';
-                    echo '</form>';
+                if ($j >= 7) {
+                    // 7以上は表示しない
+                    echo '<img src="img/iconover.png" width="12%" height=95%" class="usericon">';
+                    break;
                 }
-            echo '</table>';
+                echo '<img src="', $ic['icon_name'], '" width="12%" height=95%" class="usericon">';
 
-        ?>
+                $j++;
+            }
+            echo '</a>';
+        }
 
-        </div>
-  
-        <div class="gakugai-container">
+        echo '</td>';
+        echo '<input type="hidden" name="floor" value=', $i, '>';
+        echo '<td class="number"><button type="submit" class="floor" value="', $i, '" name="floor">', $i, '階</td>';
+        echo '</tr>';
+        echo '</form>';
+    }
+    echo '</table>';
+
+    ?>
+
+    </div>
+
+    <div class="gakugai-container">
         <h2>学外</h2>
-        </div>
-        <br>
-        <br>
-    </body>
+    </div>
+    <br>
+    <br>
+</body>
+
 </html>
