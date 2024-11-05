e317238b6f4aaae567e836a324f12a8eee45dabf
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-05 14:00:59 +0900

    ann_his

---
+++ src/announce_his.php
@@ -0,0 +1,52 @@
+<?php
+require 'parts/auto-login.php';
+
+
+?>
+<h1>アナウンス送信履歴</h1>
+<p>
+    <?php
+    $ann_list_sql = $pdo->prepare('SELECT * FROM Notification WHERE send_person=?');
+    $ann_list_sql->execute([$_SESSION['user']['user_id']]);
+    $ann_list_row = $ann_list_sql->fetchAll(PDO::FETCH_ASSOC);
+    if ($ann_list_row) {
+        ?>
+    <table>
+        <th>タイトル</th>
+        <th>送信先</th>
+        <th>送信日時</th>
+    </table>
+    <?php
+    foreach ($ann_list_row as $ann_row) {
+        $announcement_id = $ann_row['announcement_id'];
+        $title = $ann_row['title'];
+        $content = $ann_row['content'];
+        $sent_tag_id = $ann_row['sent_tag'];
+        $send_time = $ann_row['sending_time'];
+        $tag_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
+        $tag_sql->execute([$sent_tag_id]);
+        $tag_row = $tag_sql->fetch(PDO::FETCH_ASSOC);
+        $sent_tag_name = $tag_row['tag_name'];
+        ?>
+        <tr>
+            <td><?php echo $title; ?></td>
+            <td><?php echo $sent_tag_name; ?></td>
+            <td><?php echo $send_time; ?></td>
+            <form action="announce_his_info.php" method="post">
+                <input type="hidden" name="announcement_id" value=<?php echo $announcement_id; ?>>
+                <input type="submit" value="詳細">
+            </form>
+        </tr>
+        <?php
+    }
+    } else {
+        echo '<span>送信したアナウンスがありません</span>';
+    }
+    ?>
+</p>
+<?php
+
+
+
+
+?>
