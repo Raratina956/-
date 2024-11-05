aa18ac0e4b64de50393893c3cb8ba03d955694a1
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-05 21:40:43 +0900

    ana

--- src/announce_his.php
+++ src/announce_his.php
@@ -2,6 +2,8 @@
 require 'parts/auto-login.php';
 
 $announcements = [];
+$ann_send_list_row = [];
+$ann_sent_list_row = [];
 
 $ann_send_list_sql = $pdo->prepare('SELECT * FROM Notification WHERE send_person=?');
 $ann_send_list_sql->execute([$_SESSION['user']['user_id']]);
@@ -21,50 +23,50 @@ if ($ann_check_list_row) {
     }
 }
 
-// if ($ann_send_list_row || $ann_sent_list_row) {
-//     if ($ann_send_list_row) {
-//         foreach ($ann_send_list_row as $ann_row) {
-//             $send_user_id = $ann_row['send_person'];
-//             $sent_tag_id = $ann_row['sent_tag'];
-//             $user_sql = $pdo->prepare('SELECT * FROM Users WHERE user_id=?');
-//             $user_sql->execute([$send_user_id]);
-//             $user_row = $user_sql->fetch(PDO::FETCH_ASSOC);
-//             $tag_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
-//             $tag_sql->execute([$sent_tag_id]);
-//             $tag_row = $tag_sql->fetch(PDO::FETCH_ASSOC);
-//             $announcements[] = [
-//                 'announcement_id' => $ann_row['announcement_id'],
-//                 'title' => $ann_row['title'],
-//                 'content' => $ann_row['content'],
-//                 'send_user_name' => $user_row['user_name'],
-//                 'sent_tag_name' => $tag_row['tag_name'],
-//                 'send_time' => $ann_row['sending_time'],
-//                 'ann_type' => 1,
-//             ];
-//         }
-//     }
-//     if ($ann_sent_list_row) {
-//         foreach ($ann_sent_list_row as $ann_row) {
-//             $send_user_id = $ann_row['send_person'];
-//             $sent_tag_id = $ann_row['sent_tag'];
-//             $user_sql = $pdo->prepare('SELECT * FROM Users WHERE user_id=?');
-//             $user_sql->execute([$send_user_id]);
-//             $user_row = $user_sql->fetch(PDO::FETCH_ASSOC);
-//             $tag_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
-//             $tag_sql->execute([$sent_tag_id]);
-//             $tag_row = $tag_sql->fetch(PDO::FETCH_ASSOC);
-//             $announcements[] = [
-//                 'announcement_id' => $ann_row['announcement_id'],
-//                 'title' => $ann_row['title'],
-//                 'content' => $ann_row['content'],
-//                 'send_user_name' => $user_row['user_name'],
-//                 'sent_tag_name' => $tag_row['tag_name'],
-//                 'send_time' => $ann_row['sending_time'],
-//                 'ann_type' => 2,
-//             ];
-//         }
-//     }
-// } else {
-//     echo '<span>送信したアナウンスがありません</span>';
-// }
+if ($ann_send_list_row || $ann_sent_list_row) {
+    if ($ann_send_list_row) {
+        foreach ($ann_send_list_row as $ann_row) {
+            $send_user_id = $ann_row['send_person'];
+            $sent_tag_id = $ann_row['sent_tag'];
+            $user_sql = $pdo->prepare('SELECT * FROM Users WHERE user_id=?');
+            $user_sql->execute([$send_user_id]);
+            $user_row = $user_sql->fetch(PDO::FETCH_ASSOC);
+            $tag_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
+            $tag_sql->execute([$sent_tag_id]);
+            $tag_row = $tag_sql->fetch(PDO::FETCH_ASSOC);
+            $announcements[] = [
+                'announcement_id' => $ann_row['announcement_id'],
+                'title' => $ann_row['title'],
+                'content' => $ann_row['content'],
+                'send_user_name' => $user_row['user_name'],
+                'sent_tag_name' => $tag_row['tag_name'],
+                'send_time' => $ann_row['sending_time'],
+                'ann_type' => 1,
+            ];
+        }
+    }
+    if ($ann_sent_list_row) {
+        foreach ($ann_sent_list_row as $ann_row) {
+            $send_user_id = $ann_row['send_person'];
+            $sent_tag_id = $ann_row['sent_tag'];
+            $user_sql = $pdo->prepare('SELECT * FROM Users WHERE user_id=?');
+            $user_sql->execute([$send_user_id]);
+            $user_row = $user_sql->fetch(PDO::FETCH_ASSOC);
+            $tag_sql = $pdo->prepare('SELECT * FROM Tag_list WHERE tag_id=?');
+            $tag_sql->execute([$sent_tag_id]);
+            $tag_row = $tag_sql->fetch(PDO::FETCH_ASSOC);
+            $announcements[] = [
+                'announcement_id' => $ann_row['announcement_id'],
+                'title' => $ann_row['title'],
+                'content' => $ann_row['content'],
+                'send_user_name' => $user_row['user_name'],
+                'sent_tag_name' => $tag_row['tag_name'],
+                'send_time' => $ann_row['sending_time'],
+                'ann_type' => 2,
+            ];
+        }
+    }
+} else {
+    echo '<span>送信したアナウンスがありません</span>';
+}
 ?>
