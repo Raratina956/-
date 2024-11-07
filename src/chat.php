520c482356a654d4377d2f6ff6de707404f5ef0e
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-07 12:39:59 +0900

    caht

--- src/chat.php
+++ src/chat.php
@@ -85,27 +85,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $debugMessage = "デバッグメッセージ1";
             echo "<script>alert('" . addslashes($debugMessage) . "');</script>";
             foreach ($ann_row as $ann_list) {
-
-                $debugMessage = "デバッグメッセージ2";
-                echo "<script>alert('" . addslashes($debugMessage) . "');</script>";
-
                 $message_id_check = $ann_list['message_id'];
                 $mess_sql = $pdo->prepare('SELECT * FROM Message WHERE message_id = ? ORDER BY message_id DESC');
                 $mess_sql->execute([$message_id_check]);
                 $mess_row = $mess_sql->fetchAll(PDO::FETCH_ASSOC);
                 if ($mess_row) {
-                    $debugMessage = "デバッグメッセージ3";
-                    echo "<script>alert('" . addslashes($debugMessage) . "');</script>";
                     foreach ($mess_row as $mess_list) {
-                        $debugMessage = "デバッグメッセージ4";
-                        echo "<script>alert('" . addslashes($debugMessage) . "');</script>";
                         $send_id_check = $mess_list['send_id'];
                         $sent_id_check = $mess_list['sent_id'];
                         if ($send_id_check == $send_id and $sent_id == $sent_id) {
-                            $debugMessage = "デバッグメッセージ5";
-                            echo "<script>alert('" . addslashes($debugMessage) . "');</script>";
                             $info_up_sql = $pdo->prepare('UPDATE Announce_check SET message_id=?, read_check=? WHERE message_id=? AND type=?');
-                            $info_up_sql->execute([$message_id_check, 0, $message_id, 3]);
+                            $info_up_sql->execute([$message_id, 0, $message_id_check, 3]);
                         }
                     }
                 }
