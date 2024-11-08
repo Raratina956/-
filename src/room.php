945ebd03a085f69910ab7bf3063c137b229be14f
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-11-08 12:22:41 +0900

    room up

--- src/room.php
+++ src/room.php
@@ -90,7 +90,7 @@ if ($update_id == 1) {
         <h1><?php echo htmlspecialchars($floor); ?>階</h1>
         <span><?php echo '<div class="heya">', htmlspecialchars($room_name), '</div>'; ?></span>
         <div style="display:inline-flex;">
-        <ul>
+        <ul class="ul">
         <?php
         // 現在の位置情報を取得するクエリ
         $point = $pdo->prepare('SELECT * FROM Current_location WHERE user_id=?');
