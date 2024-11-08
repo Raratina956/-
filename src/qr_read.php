2749276741e7bd8e398997472c6e37307cb4e2e1
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-11-08 10:30:03 +0900

    qr

--- src/qr_read.php
+++ src/qr_read.php
@@ -9,7 +9,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QRコード読み取り</title>
-    <link rel="stylesheet" href="css/qr_read.css">
+    <link rel="stylesheet" href="css/qr_read.css" media="screen and (min-width: 1280px)">
+    <link rel="stylesheet" href="mob_css/qr_read.css" media="screen and (max-width: 480px)">
 </head>
 
 <body>
