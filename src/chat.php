5a373e8eaa1793c9fdd717178db3452316e15a81
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-11 12:08:19 +0900

    info

--- src/chat.php
+++ src/chat.php
@@ -66,13 +66,13 @@ $read_sql->execute([$_SESSION['user']['user_id'], 3]);
 $read_row = $read_sql->fetchAll(PDO::FETCH_ASSOC);
 if ($read_row) {
     foreach ($read_row as $read_list) {
-        $send_id_check = $mess_list['send_id'];
-        $sent_id_check = $mess_list['sent_id'];
         $message_id = $read_list['message_id'];
         $mess_sql = $pdo->prepare('SELECT * FROM Message WHERE message_id=?');
         $mess_sql->execute(params: [$message_id]);
-        $send_id_check = $mess_list['send_id'];
-        $sent_id_check = $mess_list['sent_id'];
+        foreach ($mess_row as $mess_list) {
+            $send_id_check = $mess_list['send_id'];
+            $sent_id_check = $mess_list['sent_id'];
+        }
         if ($send_id_check == $send_id and $sent_id == $sent_id) {
             $info_up = $pdo->prepare('UPDATE Announce_check SET read_check=? WHERE message_id=?');
             $info_up->execute([1, $message_id]);
