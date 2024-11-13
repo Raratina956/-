eee44ab86ffae04636fafdf4ed2f25bed761d146
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-11-13 11:05:23 +0900

    mapup

--- src/map.php
+++ src/map.php
@@ -165,8 +165,8 @@ unset($_SESSION['floor']['kai']);
         echo '</div>';
         echo '<form name="floor" action="floor.php" method="post">';
         echo '<input type="hidden" name="floor" value=', $i, '>';
-        // echo '<td class="number"><button type="submit" class="floor" value="', $i, '" name="floor">', $i, '階</button></td>'; // 修正: buttonタグを閉じる位置
-        echo '<td class="number">test</td>'; // 修正: buttonタグを閉じる位置
+        echo '<td class="number"><button type="submit" class="floor" value="', $i, '" name="floor">', $i, '階</button></td>'; // 修正: buttonタグを閉じる位置
+        // echo '<td class="number">test</td>'; // 修正: buttonタグを閉じる位置
         echo '</tr>';
         echo '</form>';
     }
