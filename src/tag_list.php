edbf48cac6145ae40592cb0502b519abf9d662b2
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-11-21 11:52:43 +0900

    font up

--- src/tag_list.php
+++ src/tag_list.php
@@ -22,13 +22,13 @@ if (isset($_POST['tag_id'])) {
 <link rel="stylesheet" href="mob_css/tag_list-mob.css" media="screen and (max-width: 480px)">
 <link rel="stylesheet" href="css/tag_list.css" media="screen and (min-width: 1280px)">
 <!-- font -->
-<!-- <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&display=swap" rel="stylesheet"> -->
-
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
+<!-- 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet"> -->
 
 <div class="center">
 <h1>みんなのタグ</h1>
