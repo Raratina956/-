5315cbfda6f5c4cc87756af99db1cc38865166b5
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-11-21 11:59:01 +0900

    font up

--- src/Sign-up-input.php
+++ src/Sign-up-input.php
@@ -10,6 +10,11 @@ require 'parts/db-connect.php';
     <title>新規会員登録</title>
     <link rel="stylesheet" href="mob_css/sign-up-mob.css" media="screen and (max-width: 480px)">
     <link rel="stylesheet" href="css/sign-up-input.css" media="screen and (min-width: 1280px)">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <h1>新規会員登録</h1>
