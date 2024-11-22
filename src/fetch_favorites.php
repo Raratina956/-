54a7f96782f4b0e469aff9aa72a6d96cd2f663fc
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-22 10:20:33 +0900

    favorit

--- src/fetch_favorites.php
+++ src/fetch_favorites.php
@@ -43,7 +43,7 @@ switch ($type) {
 echo '<style> table tr { border-bottom: 1px solid #000; /* 下線を追加 */ } table tr:last-child { border-bottom: none; /* 最後の行の下線を除去 */ } </style>';
 // 取得したデータを表示
 if ($list_raw) {
-    echo '<table border="0" style="font-size: 16pt;">';
+    echo '<table border="1" style="font-size: 16pt;">';
     foreach ($list_raw as $favorite) {
     echo '<tr>';
     $follower_id =  $favorite['follower_id'];
