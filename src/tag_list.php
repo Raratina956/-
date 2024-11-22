9a73950b9adf0c9b30ceecc385252bd7698dc0ad
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-22 10:01:06 +0900

    tag_list_css

--- src/tag_list.php
+++ src/tag_list.php
@@ -27,15 +27,16 @@ if (isset($_POST['tag_id'])) {
 <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
 
 <div class="center">
-<h1>みんなのタグ</h1>
-<a href="join_tag.php" class="join_tag"><span>参加しているタグはこちら</span></a>
-<form action="tag_list" method="post">
-    <input type="text" name="tag_search" class="textbox" placeholder="検索したい内容を入力してください">
-    <input type="submit" value="検索" class="search">
-</form>
+    <h1>みんなのタグ</h1>
+    <a href="join_tag.php" class="join_tag"><span>参加しているタグはこちら</span></a>
+    <form action="tag_list" method="post">
+        <input type="text" name="tag_search" class="textbox" placeholder="検索したい内容を入力してください">
+        <input type="submit" value="検索" class="search">
+    </form>
 </div>
 <?php
-function limitDisplay($text, $limit) {
+function limitDisplay($text, $limit)
+{
     // Check if the text exceeds the limit
     if (mb_strlen($text) > $limit) {
         // Return the limited text with ellipsis
@@ -58,7 +59,7 @@ if (isset($_POST['tag_search'])) {
         $search_sql->execute([$tag_search]);
         $results = $search_sql->fetchAll(PDO::FETCH_ASSOC);
         $judge = 0;
-        echo '<center>【',$tag_search,'】の検索結果</center>';
+        echo '<center>【', $tag_search, '】の検索結果</center>';
     }
 
 } else {
@@ -72,45 +73,47 @@ if ($results) {
     <br>
     <a href="map.php" class="back-link">戻る</a>
     <br>
-    <table id="table" border="0">
-        <th>タグ名</th>
-        <th>参加人数</th>
-        <th>作成者</th>
-        <th></th>
-        <?php
-foreach ($results as $row) {
-    $sql_count = $pdo->prepare('SELECT COUNT(DISTINCT user_id) AS user_count FROM Tag_attribute WHERE tag_id = ?');
-    $sql_count->execute([$row['tag_id']]);
-    $count_result = $sql_count->fetch(PDO::FETCH_ASSOC);
-    $user_count = $count_result['user_count'];
-    if ($user_count != 0 || $judge == 0) {
-        echo '<tr>';
-        echo '<td>', limitDisplay($row['tag_name'], 10), '</td>';
-        echo '<td>', $user_count, '</td>';
-        $sql_user = $pdo->prepare('SELECT * FROM Users WHERE user_id=?');
-        $sql_user->execute([$row['user_id']]);
-        $row_user = $sql_user->fetch();
-        echo '<td>', limitDisplay($row_user['user_name'], 10), '</td>';
-        echo '<form action="tag_list.php" method="post">';
-        echo '<input type="hidden" name="tag_id" value=', $row['tag_id'], '>';
-        if (!(empty($_POST['tag_search']))) {
-            echo '<input type="hidden" name="tag_search" value="', $tag_search, '">';
-        }
-        $sql = $pdo->prepare('SELECT * FROM Tag_attribute WHERE tag_id=? AND user_id=?');
-        $sql->execute([$row['tag_id'], $_SESSION['user']['user_id']]);
-        $row = $sql->fetch(PDO::FETCH_ASSOC);
-        if (!$row) {
-            echo '<td><input type="submit" value="参加" class="join"></td>';
-        } else {
-            echo '<td><input type="submit" value="参加済" class="joined"></td>';
-        }
-        echo '</form>';
-        echo '</tr>';
-    }
-}
-?>
+    <div class="tag_list_table">
+        <table id="table" border="0">
+            <th>タグ名</th>
+            <th>参加人数</th>
+            <th>作成者</th>
+            <th></th>
+            <?php
+            foreach ($results as $row) {
+                $sql_count = $pdo->prepare('SELECT COUNT(DISTINCT user_id) AS user_count FROM Tag_attribute WHERE tag_id = ?');
+                $sql_count->execute([$row['tag_id']]);
+                $count_result = $sql_count->fetch(PDO::FETCH_ASSOC);
+                $user_count = $count_result['user_count'];
+                if ($user_count != 0 || $judge == 0) {
+                    echo '<tr>';
+                    echo '<td>', limitDisplay($row['tag_name'], 10), '</td>';
+                    echo '<td>', $user_count, '</td>';
+                    $sql_user = $pdo->prepare('SELECT * FROM Users WHERE user_id=?');
+                    $sql_user->execute([$row['user_id']]);
+                    $row_user = $sql_user->fetch();
+                    echo '<td>', limitDisplay($row_user['user_name'], 10), '</td>';
+                    echo '<form action="tag_list.php" method="post">';
+                    echo '<input type="hidden" name="tag_id" value=', $row['tag_id'], '>';
+                    if (!(empty($_POST['tag_search']))) {
+                        echo '<input type="hidden" name="tag_search" value="', $tag_search, '">';
+                    }
+                    $sql = $pdo->prepare('SELECT * FROM Tag_attribute WHERE tag_id=? AND user_id=?');
+                    $sql->execute([$row['tag_id'], $_SESSION['user']['user_id']]);
+                    $row = $sql->fetch(PDO::FETCH_ASSOC);
+                    if (!$row) {
+                        echo '<td><input type="submit" value="参加" class="join"></td>';
+                    } else {
+                        echo '<td><input type="submit" value="参加済" class="joined"></td>';
+                    }
+                    echo '</form>';
+                    echo '</tr>';
+                }
+            }
+            ?>
 
-    </table>
+        </table>
+    </div>
     <?php
 } else {
     echo '<br><center>タグがありません</center>';
@@ -118,4 +121,3 @@ foreach ($results as $row) {
     echo '<a href="map.php" class="back-link">マップへ</a>';
 }
 ?>
-
