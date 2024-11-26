8950b3501f293d52e65f72bbb8aa6c0ff7f2cf78
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-26 14:05:35 +0900

    fghj

--- src/floor.php
+++ src/floor.php
@@ -58,7 +58,7 @@ require 'header.php';
         if (!$isMobile) {
             $icon['icon_name'] = 'img/icon/default.jpg';
             echo '<br>';
-            echo '<img src="', $icon['icon_name'], '" width="20%" height="20%" class="usericon">';
+            echo '<img src="', $icon['icon_name'], '" width="12%" height="95%" class="usericon">';
         }
         echo '</li>';
     }
