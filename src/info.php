ce82b99f2d00d14d94fd42cba86afc885288b14c
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-26 10:03:28 +0900

    info

--- src/info.php
+++ src/info.php
@@ -525,7 +525,7 @@ require 'header.php';
                         echo '</tr>';
                         echo '<tr>';
                         echo '<td class="day" style="width: 10%;">', timeAgo($logtime), '</td>';
-                        echo '<td colspan="2"class="title" style="width: 60%;"> 件名：', $title, '</td>';
+                        echo '<td colspan="2" style="width: 60%;"> 件名：', $title, '</td>';
                         ?>
                         <?php
                         if ($read_check == 0) {
