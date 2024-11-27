18b7786ccc285489104a099ef37ae3754f71f625
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-11-27 09:47:20 +0900

    CSS

--- src/header.php
+++ src/header.php
@@ -34,7 +34,7 @@ if (isset($_POST['logout'])) {
     <link rel="stylesheet" href="css/header.css" media="screen and (min-width: 481px) and (max-width: 1279px)">
     <link rel="stylesheet" type="text/css" href="css/header.css" media="screen and (min-width: 1280px)">
     <link rel="stylesheet" href="mob_css/humberger-mob.css" media="screen and (max-width: 480px)">
-    <link rel="stylesheet" href="css/humberger-mob.css" media="screen and (min-width: 481px) and (max-width: 1279px)">
+    <link rel="stylesheet" href="css/test.css" media="screen and (min-width: 481px) and (max-width: 1279px)">
     <link rel="stylesheet" href="css/test.css" media="screen and (min-width: 1280px)">
     <link rel="icon" href="img/pin.png" sizes="32x32" type="image/png">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=0.8"> -->
