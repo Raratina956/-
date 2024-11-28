8fb62ea3b67750ddcdefc74ed819bedc222b1ace
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-11-28 12:09:09 +0900

    up

--- src/map.php
+++ src/map.php
@@ -132,7 +132,7 @@ unset($_SESSION['floor']['kai']);
         echo '</form></div>';
 
         // 学外
-        echo '<div class="gakugai-container"><li><a  href="mapindex.php"><img src="img/gakugai.png" width="150px" height="150px"></a></li><li><a style="position: absolute;top: 117px;left: 30px;">学外はこちら</a></li></div>';
+        echo '<div class="gakugai-container"><li><a  href="mapindex.php"><img class="g-img" src="img/gakugai.png" width="150px" height="150px"></a></li><li><a style="position: absolute;top: 117px;left: 30px;">学外はこちら</a></li></div>';
 
         //  map
         echo '<br><table class="table">';
