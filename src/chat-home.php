643a85939aeac48d75f4a4b7849b34d84f651afa
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-11-29 10:20:32 +0900

    Update chat-home.php

--- src/chat-home.php
+++ src/chat-home.php
@@ -77,8 +77,9 @@ function getUnreadMessageCount($pdo, $user_id, $partner_id) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>チャット</title>
-    <link rel="stylesheet" href="mob_css/chat-home-mob.css" media="screen and (max-width: 480px)">
-    <link rel="stylesheet" href="css/chat.css" media="screen and (min-width: 1280px)">
+    <link rel="stylesheet" href="mob_css/map-mob.css" media="screen and (max-width: 480px)">
+    <link rel="stylesheet" href="css/map.css" media="screen and (min-width: 481px) and (max-width: 1279px)">
+    <link rel="stylesheet" href="css/map.css" media="screen and (min-width: 1280px)">
     <!-- font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
