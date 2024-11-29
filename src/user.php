dc816f44f899df83ec10425831288a6c60e28b4d
miyakou1122 <per.miyakou1122@gmail.com>
2024-11-29 11:12:18 +0900

    user

--- src/user.php
+++ src/user.php
@@ -213,7 +213,7 @@ require 'header.php';
                     echo 'クラス：', $classtagname['classtag_name'], '<br>';
                     echo $user['mail_address'], "<br>";
                     $current_sql = $pdo->prepare('SELECT * FROM Current_location WHERE user_id=?');
-                    $current_sql->execute([$_SESSION['user']['user_id']]);
+                    $current_sql->execute([$_GET['user_id']]);
                     $current_row = $current_sql->fetch();
                     if ($current_row) {
                         $room_id = $current_row['classroom_id'];
@@ -235,7 +235,7 @@ require 'header.php';
                     echo 'クラス：クラスが設定されていません', '<br>';
                     echo $user['mail_address'], "<br>";
                     $current_sql = $pdo->prepare('SELECT * FROM Current_location WHERE user_id=?');
-                    $current_sql->execute([$_SESSION['user']['user_id']]);
+                    $current_sql->execute([$_GET['user_id']]);
                     $current_row = $current_sql->fetch();
                     if ($current_row) {
                         $room_id = $current_row['classroom_id'];
@@ -257,7 +257,7 @@ require 'header.php';
                 echo '名前：', $user['user_name'], "先生<br>";
                 echo $user['mail_address'], "<br>";
                 $current_sql = $pdo->prepare('SELECT * FROM Current_location WHERE user_id=?');
-                $current_sql->execute([$_SESSION['user']['user_id']]);
+                $current_sql->execute([$_GET['user_id']]);
                 $current_row = $current_sql->fetch();
                 if ($current_row) {
                     $room_id = $current_row['classroom_id'];
