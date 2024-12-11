bcf3ebf0dc2f30bfc1a9f881490557a223fe0da7
miyakou1122 <per.miyakou1122@gmail.com>
2024-12-11 14:30:20 +0900

    map

--- src/update-location.php
+++ src/update-location.php
@@ -24,9 +24,12 @@ $info_search_row = $info_search_sql->fetch();
 
 if ($info_search_row) {
     // 既存のデータがあれば更新
+    $get_primary_key = $pdo->prepare('SELECT id FROM Current_location WHERE user_id = ?');
+    $get_primary_key->execute([$user_id]);
+    $current_location_id = $get_primary_key->fetchColumn();
     $info_update = $pdo->prepare('UPDATE Current_location SET classroom_id = ?, position_info_id = ?, logtime = ? WHERE user_id = ?');
     $info_update->execute([null, 1, $current_datetime, $user_id]);
-    
+
 } else {
     // データがなければ挿入
     $info_insert = $pdo->prepare('INSERT INTO Current_location (user_id, position_info_id, logtime) VALUES (?, ?, ?)');
@@ -35,24 +38,24 @@ if ($info_search_row) {
 }
 
 $favorite_user = $pdo->prepare('SELECT * FROM Favorite WHERE follower_id=?');
-    $favorite_user->execute([$_SESSION['user']['user_id']]);
-    $favorite_results = $favorite_user->fetchAll(PDO::FETCH_ASSOC);
-    if ($favorite_results) {
-        foreach ($favorite_results as $favorite_row) {
-            $announce_sql = $pdo->prepare('SELECT * FROM Announce_check WHERE user_id=? AND type=?');
-            $announce_sql->execute([
-                $favorite_row['follow_id'],
-                2
-            ]);
-            if ($announce_sql->rowCount() == 0) {
-                $new_announce = $pdo->prepare('INSERT INTO Announce_check(current_location_id, user_id, read_check, type) VALUES (?, ?, ?, ?)');
-                $new_announce->execute([$current_location_id, $favorite_row['follow_id'], 0, 2]);
-            } else {
-                $update_announce = $pdo->prepare('UPDATE Announce_check SET current_location_id=?, read_check=? WHERE user_id=? AND type=?');
-                $update_announce->execute([$current_location_id, 0, $favorite_row['follow_id'], 2]);
-            }
+$favorite_user->execute([$_SESSION['user']['user_id']]);
+$favorite_results = $favorite_user->fetchAll(PDO::FETCH_ASSOC);
+if ($favorite_results) {
+    foreach ($favorite_results as $favorite_row) {
+        $announce_sql = $pdo->prepare('SELECT * FROM Announce_check WHERE user_id=? AND type=?');
+        $announce_sql->execute([
+            $favorite_row['follow_id'],
+            2
+        ]);
+        if ($announce_sql->rowCount() == 0) {
+            $new_announce = $pdo->prepare('INSERT INTO Announce_check(current_location_id, user_id, read_check, type) VALUES (?, ?, ?, ?)');
+            $new_announce->execute([$current_location_id, $favorite_row['follow_id'], 0, 2]);
+        } else {
+            $update_announce = $pdo->prepare('UPDATE Announce_check SET current_location_id=?, read_check=? WHERE user_id=? AND type=?');
+            $update_announce->execute([$current_location_id, 0, $favorite_row['follow_id'], 2]);
         }
     }
+}
 
 
 // 成功レスポンスを返す
